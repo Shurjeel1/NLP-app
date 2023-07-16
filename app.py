a0b4a59ace6dec3f5a1cc5441e9a4d7d4e69db3c
M SHARJEEL JAVAID <126756480+Shurjeel1@users.noreply.github.com>
2023-07-16 19:23:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,139 +1,45 @@
-# Core Pkgs
 import streamlit as st
-import altair as alt
-import plotly.express as px
+import requests
+from bs4 import BeautifulSoup
 
-# EDA Pkgs
-import pandas as pd
-import numpy as np
-from datetime import datetime
-import sqlite3
-import joblib
+# Function to scrape data from a URL
+def scrape_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Specify the file path using raw string or escape the backslashes
-file_path = r"C:\Users\dj\OneDrive\Desktop\NLP Project\App\models\emotion_classifier_pipe_lr.pkl"
+    quotes = soup.find_all('div', class_='quote')
 
-# Load the file using joblib
-try:
-    pipe_lr = joblib.load(file_path)
-    # Perform any necessary operations with the loaded file
-    print("File loaded successfully!")
-except FileNotFoundError:
-    print("File not found. Please ensure the file exists at the specified location.")
-except Exception as e:
-    print("An error occurred while loading the file:", str(e))
+    scraped_data = []
 
-# Utils
+    for quote in quotes:
+        text = quote.find('span', class_='text').text
+        author = quote.find('small', class_='author').text
+        scraped_data.append({'Author': author, 'Quote': text})
 
-# Load Database Pkg
-conn = sqlite3.connect('C:/Users/dj/OneDrive/Desktop/NLP Project/App/database.db')
-c = conn.cursor()
+    return scraped_data
 
-# Fxn
-def create_page_visited_table():
-    c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT,timeOfvisit TIMESTAMP)')
-
-def add_page_visited_details(pagename, timeOfvisit):
-    c.execute('INSERT INTO pageTrackTable(pagename,timeOfvisit) VALUES(?,?)', (pagename,timeOfvisit))
-    conn.commit()
-
-def view_all_page_visited_details():
-    c.execute('SELECT * FROM pageTrackTable')
-    data = c.fetchall()
-    return data
-
-def create_emotionclf_table():
-    c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
-
-def add_prediction_details(rawtext, prediction, probability, timeOfvisit):
-    c.execute('INSERT INTO emotionclfTable(rawtext,prediction,probability,timeOfvisit) VALUES(?,?,?,?)', (rawtext,prediction,probability,timeOfvisit))
-    conn.commit()
-
-def view_all_prediction_details():
-    c.execute('SELECT * FROM emotionclfTable')
-    data = c.fetchall()
-    return data
-
-emotions_emoji_dict = {"anger":"😠","disgust":"🤮", "fear":"😨😱", "happy":"🤗", "joy":"😂", "neutral":"😐", "sad":"😔", "sadness":"😔", "shame":"😳", "surprise":"😮"}
-
-# Main Application
+# Streamlit application code
 def main():
-    st.title("Emotion Classifier App")
-    menu = ["Home","Monitor","About"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    st.title("Web Scraping Application")
 
-    create_page_visited_table()
-    create_emotionclf_table()
+    # User input for the URL to scrape
+    url = st.text_input("Enter the URL to scrape:")
 
-    if choice == "Home":
-        add_page_visited_details("Home", datetime.now())
-        st.subheader("Home-Emotion In Text")
+    # Scrape button
+    if st.button("Scrape"):
+        # Check if the URL is provided
+        if url:
+            # Call the scrape_data function to perform scraping
+            scraped_data = scrape_data(url)
 
-        with st.form(key='emotion_clf_form'):
-            raw_text = st.text_area("Type Here")
-            submit_text = st.form_submit_button(label='Submit')
+            # Display the scraped data
+            st.write("Scraped Data:")
+            for data in scraped_data:
+                st.write(f"Author: {data['Author']}")
+                st.write(f"Quote: {data['Quote']}")
+                st.write("---")
+        else:
+            st.write("Please enter a URL to scrape.")
 
-        if submit_text:
-            col1, col2 = st.columns(2)
-
-            # Apply Fxn Here
-            prediction = predict_emotions(raw_text)
-            probability = get_prediction_proba(raw_text)
-            
-            add_prediction_details(raw_text, prediction, np.max(probability), datetime.now())
-
-            with col1:
-                st.success("Original Text")
-                st.write(raw_text)
-
-                st.success("Prediction")
-                emoji_icon = emotions_emoji_dict[prediction]
-                st.write("{}:{}".format(prediction, emoji_icon))
-                st.write("Confidence:{}".format(np.max(probability)))
-
-            with col2:
-                st.success("Prediction Probability")
-                proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
-                proba_df_clean = proba_df.T.reset_index()
-                proba_df_clean.columns = ["emotions", "probability"]
-
-                fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
-                st.altair_chart(fig, use_container_width=True)
-
-    elif choice == "Monitor":
-        add_page_visited_details("Monitor", datetime.now())
-        st.subheader("Monitor App")
-
-        with st.expander("Page Metrics"):
-            page_visited_details = pd.DataFrame(view_all_page_visited_details(), columns=['Pagename','Time_of_Visit'])
-            st.dataframe(page_visited_details)    
-
-            pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
-            c = alt.Chart(pg_count).mark_bar().encode(x='Pagename', y='Counts', color='Pagename')
-            st.altair_chart(c, use_container_width=True)   
-
-            p = px.pie(pg_count, values='Counts', names='Pagename')
-            st.plotly_chart(p, use_container_width=True)
-
-        with st.expander('Emotion Classifier Metrics'):
-            df_emotions = pd.DataFrame(view_all_prediction_details(), columns=['Rawtext', 'Prediction', 'Probability', 'Time_of_Visit'])
-            st.dataframe(df_emotions)
-
-            prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
-            pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction', y='Counts', color='Prediction')
-            st.altair_chart(pc, use_container_width=True)
-
-    else:
-        st.subheader("About")
-        add_page_visited_details("About", datetime.now())
-
-def predict_emotions(docx):
-    results = pipe_lr.predict([docx])
-    return results[0]
-
-def get_prediction_proba(docx):
-    results = pipe_lr.predict_proba([docx])
-    return results
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
